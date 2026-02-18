4986279983498e52731297c236e126d6ce9e23f9
Firdaus <firdausmustar@gmail.com>
2026-02-18 22:57:50 +0800

    change url

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,6 @@ import tailwind from '@astrojs/tailwind';
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  site: 'https://nina.github.io', // Placeholder, will be adjusted for GitHub Pages
-  base: '/solat-app-intro', // Repository name for GitHub Pages
+  site: 'https://stardaus.github.io',
+  base: '/solat-app-intro',
 });
